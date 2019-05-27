ee995c57305eb97c7260c25be00c247f8bd834f5
dianabejan <diana.bejan@student.utcluj.ro>
2019-05-27 15:37:42 +0300

    Revert "redesign (#1)"
    This reverts commit 8534515b8b1fa4c3469830cd2d54562a04b80495.

---
+++ bitstored/src/App.vue
@@ -0,0 +1,46 @@
+
+<template>
+  <default-layout>
+    <div class="Home">
+      <div class="container">
+        <first-page></first-page>
+      </div>
+    </div>
+  </default-layout>
+</template>
+
+<script>
+import FirstPage from "./pages/FirstPage";
+import DefaultLayout from "./layouts/DefaultLayout";
+
+export default {
+  name: "App",
+  components: {
+    FirstPage,
+    DefaultLayout
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#logo {
+  max-width: 30%;
+  height: auto;
+}
+.catalog {
+  display: flex;
+}
+
+.catalog {
+  flex: 3;
+  order: 2;
+}
+</style>
---
+++ run.sh
@@ -0,0 +1,2 @@
+ cd bitstored
+npm run dev
